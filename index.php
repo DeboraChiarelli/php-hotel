147bbe2fecafa53632a5fa5cedaf09f8c5395f87
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-11-22 14:41:15 +0100

    Add - stile con Bootstrap

--- index.php
+++ index.php
@@ -59,3 +59,49 @@ Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di fi
         echo "<hr>";
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container mt-5">
+    <h1>Hotel Informations</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach ($hotels as $hotel) {
+                    echo "<tr>";
+                    echo "<td>{$hotel['name']}</td>";
+                    echo "<td>{$hotel['description']}</td>";
+                    echo "<td>" . ($hotel['parking'] ? 'Available' : 'Not available') . "</td>";
+                    echo "<td>{$hotel['vote']}</td>";
+                    echo "<td>{$hotel['distance_to_center']}</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
+
+
+
+
+</body>
+</html>
